307005057492b259ced9ff0655979383d0d3c11c
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2022-02-22 12:32:47 +0100

    Added an scheduled job with the hello world service.

---
+++ impl/src/java/com/edf/helloworld/impl/jobs/HelloWorldJob.java
@@ -0,0 +1,22 @@
+package com.edf.helloworld.impl.jobs;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import com.edf.helloworld.api.HelloWorldService;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HelloWorldJob implements Job {
+
+    @Setter
+    private HelloWorldService helloWorldService;
+
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        log.info("Hello world job, we have {} persons.", helloWorldService.countPersons());
+    }
+
+}
