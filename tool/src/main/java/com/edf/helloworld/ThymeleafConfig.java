0a519bff49244f197b27083c0d8078748c8c0dec
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2022-05-09 12:02:35 +0200

    Make it work in Sakai 20

--- tool/src/main/java/com/edf/helloworld/ThymeleafConfig.java
+++ tool/src/main/java/com/edf/helloworld/ThymeleafConfig.java
@@ -17,6 +17,7 @@
 package com.edf.helloworld;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import org.sakaiproject.util.ResourceLoaderMessageSource;
 import org.springframework.context.ApplicationContext;
@@ -25,13 +26,29 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.ISpringTemplateEngine;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.spring4.ISpringTemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
@@ -79,4 +96,109 @@ public class ThymeleafConfig implements WebMvcConfigurer, ApplicationContextAwar
         return templateResolver;
     }
 
+    public MessageCodesResolver getMessageCodesResolver() {
+        return null;
+    }
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureAsyncSupport(AsyncSupportConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Validator getValidator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
